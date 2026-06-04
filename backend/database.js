6f1b64137ee4757651a5c8b62886b3c9a058c256
Jansen Muniz <jansen_muniz1@gmail.com>
2026-06-03 21:48:56 -0300

    atualização no database.js

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./boletos.db", (erro) => {
+const db = new sqlite3.Database("./database.db", (erro) => {
   if (erro) {
     console.error("Erro ao conectar banco:", erro.message);
   } else {
